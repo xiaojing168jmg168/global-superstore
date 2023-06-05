446451a3c6fb501baff5d0a0536eb42835c5a33b
Xiaojing Deng <8redfishcreative@gmail.com>
2023-06-05 15:52:39 -0400

    show home page , inventory page

---
+++ src/main/java/com/ltp/Item.java
@@ -0,0 +1,54 @@
+package com.ltp;
+import java.util.Date;
+
+public class Item {
+    private String category;
+    private String name;
+    private Double price;
+    private Double discount;
+    private Date date;
+
+    public Item() {
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getDiscount() {
+        return this.discount;
+    }
+
+    public void setDiscount(Double discount) {
+        this.discount = discount;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+}
---
+++ src/main/java/com/ltp/globalsuperstore/StoreControll.java
@@ -0,0 +1,19 @@
+package com.ltp.globalsuperstore;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StoreControll {
+    @GetMapping("/")
+    public String getForm(Model model){
+   
+        model.addAttribute("categories", Constants.CATEGORIES);
+        return "form";
+    };
+    @GetMapping("/inventory")
+    public String getInventory(){
+        return "inventory";
+    }
+}
