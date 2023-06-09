37ae39bf0497c6ded4b1cc753aa6afec0e920643
Xiaojing Deng <8redfishcreative@gmail.com>
2023-06-09 11:05:14 -0400

    refactory to controller, repository, service

--- src/main/java/com/ltp/globalsuperstore/StoreControll.java
+++ src/main/java/com/ltp/globalsuperstore/StoreControll.java
@@ -1,10 +1,5 @@
 package com.ltp.globalsuperstore;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
@@ -19,13 +14,11 @@ import com.ltp.Item;
 
 @Controller
 public class StoreControll {
-    List<Item> items = new ArrayList<>();
+    StoreService storeService = new StoreService();
     @GetMapping("/")
     public String getForm(Model model, @RequestParam(required = false) String id){
-        int index = getIndexFromId(id);
-        model.addAttribute("item", index == Constants.NOT_FOUND ? new Item() : items.get(index));
-   
-
+       
+    model.addAttribute("item", storeService.getItemFormId(id));
         return "form";
     };
 
@@ -35,15 +28,7 @@ public class StoreControll {
             result.rejectValue("price", "", "Price cannot be less than discount");
         }
         if(result.hasErrors()) return "form";
-        int index = getIndexFromId(item.getId());
-        String status = Constants.SUCCESS_STATUS;
-        if(index == Constants.NOT_FOUND){
-            items.add(item); 
-        }else if(within5Days(item.getDate(), items.get(index).getDate())){
-            items.set(index, item);
-        }else{
-            status = Constants.FAILED_STATUS;
-        }
+       String status = storeService.handleSubmit(item);
          // save flash attribute status: success
         redirectAttributes.addFlashAttribute("status", status);
          
@@ -53,18 +38,9 @@ public class StoreControll {
  
     @GetMapping("/inventory")
     public String getInventory(Model model){
-        model.addAttribute("items", items);
+        model.addAttribute("items", storeService.getItems());
         return "inventory";
     }
-    public int getIndexFromId(String id){
-        for(int i = 0; i < items.size(); i++){
-           if(items.get(i).getId().equals(id)) return i;
-        }
-        return Constants.NOT_FOUND;
-    }
 
-    public boolean within5Days(Date newDate, Date oldDate){
-        long diff = Math.abs(newDate.getTime() - oldDate.getTime());
-        return (int) (TimeUnit.MILLISECONDS.toDays(diff)) <= 5;
-    }
+    
 }
